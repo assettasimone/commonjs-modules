28782ac6d0240f57df50290dfcac1ced18498490
assettasimone <assettasimone@gmail.com>
2025-08-30 00:55:08 +0200

    done

--- people.js
+++ people.js
@@ -1,4 +1,24 @@
-import { hobbiesF } from "./hobbies";
+import hobbiesF from "./hobbies.js";
 
-import { namesF } from "./names"
+import namesF from "./names.js"
 
+function peoplesF() {
+    const names = namesF("Simone", "Assetta");
+    const fullName = names.firstName + ' ' + names.lastName
+
+    const hobbies = hobbiesF("warhammer", "camper", "moto")
+    const hobbiesStr = `${hobbies}`
+
+    const people = {
+        fullName,
+        hobbiesStr
+    }
+
+    console.log(people);
+
+    return people
+
+}
+
+
+peoplesF();
